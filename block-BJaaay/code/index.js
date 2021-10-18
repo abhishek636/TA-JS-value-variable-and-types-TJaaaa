12fe12786048928230c837da8a8b4a1e1eb42c5e
Abhishek Bhatt <bhattabhishek636@gmail.com>
2021-10-18 12:24:21 +0530

    1 to 10

--- block-BJaaay/code/index.js
+++ block-BJaaay/code/index.js
@@ -4,10 +4,35 @@
 
 // Using `console.log` log all the values from 1 to 10.
 
+for (let i = 1; i <= 10; i = i+1){
+    console.log(i);
+}
+
 // Using `console.log` log all the evan values from 1 to 10.
+
+for (let i = 1; i <= 10; i = i+1){
+    if (i % 2 == 0){
+        console.log(i);
+    }
+}
 
 // Using `console.log` log all the od values from 1 to 10.
 
+for (let i = 1; i <= 10; i = i+1){
+    if (i % 2 !== 0){
+        console.log(i);
+    }
+}
+
 // Calculate the sum of all numbers from 1 to 10.
 
+sum = (1+2+3+4+5+6+7+8+9+10);
+sum;
+
 // Log all the values from 1 to 10 using while loop
+
+let i = 0 ;
+while( i <= 10){
+    console.log(i);
+    i = i+1;
+}
