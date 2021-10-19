ae39589129c9d0570c7abb20fc6bfac5b0f4dfa4
Abhishek Bhatt <bhattabhishek636@gmail.com>
2021-10-19 09:31:58 +0530

    ghjkl

--- block-BJaabd/code/index.js
+++ block-BJaabd/code/index.js
@@ -11,6 +11,17 @@ Write a program that asks the user his/her age and check for the following condi
 👇
 */
 
+let age = prompt("Your age")
+if (age>=12 && age<=55) {
+  alert("You can participate in the marathon")
+} if(age>=4 && age<=11){
+  alert(" You are too young to participate in the marathon")
+} if (age<4){
+  alert(" Hey Kiddo! Can You Walk ?")
+} if (age>55){
+  alert(" You are too old to participate in the marthon")
+}
+
 // Loops
 /*
 Given a positive integer `n`. Print the word (hello) in format of heeeello (letter 'e' must be repeated `n` times). Take input from prompt and print the result in alert.
@@ -21,6 +32,9 @@ n = 7 => output: heeeeeeello
 👇
 */
 // [Your code goes here]
+
+// let n = Number(prompt("enter a number"));
+
 
 /*
 Program to calculate the sum of first n natural numbers(1,2,3...n are known as natural numbers). Prompt user to enter n (using `prompt`) then based on input provided calculate and show result in `alert`.
@@ -45,6 +59,42 @@ Take a number value from user and alert the message if it matches the conditions
 * [ ] PLEASE TRY AGAIN, if  is none of the above.
 
 */
+
+
+let num = prompt("Enter a number");
+switch (num =(`${num}`)){
+  case 1 :
+    alert('ONE');
+    break;
+  case 2 :
+    alert('TWO');
+    break;
+  case 3 :
+    alert('THREE');
+    break;
+  case 4 :
+    alert('FOUR');
+    break;
+  case 5 :
+    alert('FIVE');
+    break;
+  case 6 :
+    alert('SIX');
+    break;
+  case 7 :
+    alert('SEVEN');
+    break;
+  case 8 :
+    alert('EIGHT');
+    break;
+  case 9 :
+    alert('NINE');
+    break;
+  default:
+    alert("PLEASE TRY AGAIN")
+}
+
+
 // [Your code goes here]
 
 /*
